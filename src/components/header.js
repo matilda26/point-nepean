a6c957af5fb1e5834cbc364186ca035b3fce2542
Matilda Rutherford <matilda@studioalt.com.au>
2020-11-01 14:36:07 +1100

    Add modal and body lock

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,37 @@
+import { clearAllBodyScrollLocks, disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+import { useEffect, useRef, useState } from "react"
+
+const { default: Modal } = require("./modal")
+
 const Header = () => {
+    const [modalState, setModalState] = useState(false)
+    const targetElement = useRef()
+
+    useEffect(() => {
+        return function cleanup() {
+            clearAllBodyScrollLocks()
+        }
+    }, [])
+
+    const showModal = () => {
+        setModalState(true)
+        disableBodyScroll(targetElement);
+    } 
+
+    const hideModal = () => {
+        setModalState(false)
+        enableBodyScroll(targetElement);
+    } 
+
     return (
-        <header className="header">
-            <h2 className="header__link">Cameron Gordon</h2>
-            <h2 className="header__link">Major Project</h2>
-        </header>
+        <>
+            <header className="header">
+                <h2 onClick={showModal} className="header__link">Cameron Gordon</h2>
+                <h2 className="header__link">Major Project</h2>
+            </header>
+            <Modal modalState={modalState} closeModal={hideModal}/>
+        </>
     )
 } 
 
-export default Header
+export default Header;
